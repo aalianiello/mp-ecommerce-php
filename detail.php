a5a4d6f906c52757b9c1cfe38765e8f8255c10e6
Adriano Alianiello <adriano.alianiello@mercadolibre.com>
2019-09-18 18:37:10 -0300

    fix 7

--- detail.php
+++ detail.php
@@ -548,19 +548,20 @@
 
                                     // Crea un objeto de preferencia
                                     $preference = new MercadoPago\Preference();
-
                                     // Crea un ítem en la preferencia
                                     $item = new MercadoPago\Item();
                                     $item->title = $_POST['title'];
                                     $item->quantity = $_POST['unit'];
-                                    $item->unit_price = $_POST['price'];
+                                    $item->unit_price = $_POST['∫price'];
                                     $preference->items = array($item);
                                     $preference->save();
                                     ?>
                                     <form action="/procesar-pago" method="POST">
                                         <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>">
                                         </script>˘
+
                                     </form>
+
                                 </div>
                             </div>
                         </div>
